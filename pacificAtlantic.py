4fea0f1f60b7e865b36f749e68b3e7e4cbe82c85
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-18 21:46:28 -0600

    Update pacificAtlantic.py

--- pacificAtlantic.py
+++ pacificAtlantic.py
@@ -6,3 +6,12 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS, COLS = len(heights), len(heights[0])
         pac, atl = set(), set()
+        
+        def dfs(r, c, visited, prevHeight):
+            if ((r,c) in visited or r < 0 or c < 0 or r == ROWS or c == COLS or heights[r][c] < prevHeight):
+                return
+            visited.add((r,c))
+            dfs(r + 1, c, visited, heights[r][c])
+            dfs(r - 1, c, visited, heights[r][c])
+            dfs(r, c + 1, visited, heights[r][c])
+            dfs(r, c - 1, visited, heights[r][c])
