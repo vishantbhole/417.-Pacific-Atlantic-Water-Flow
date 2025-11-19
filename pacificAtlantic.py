3a62676b71ffe3b7493b7baedc91397a8261eaac
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-18 21:47:10 -0600

    Update pacificAtlantic.py

--- pacificAtlantic.py
+++ pacificAtlantic.py
@@ -15,3 +15,11 @@ class Solution:
             dfs(r - 1, c, visited, heights[r][c])
             dfs(r, c + 1, visited, heights[r][c])
             dfs(r, c - 1, visited, heights[r][c])
+    
+        for c in range(COLS):
+            dfs(0, c, pac, heights[0][c])
+            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+
+        for r in range(ROWS):
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
