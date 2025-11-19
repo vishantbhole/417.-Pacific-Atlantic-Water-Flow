fc8dcb5913fb33467c6cfc80c31cd507abef1d94
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-18 21:46:00 -0600

    Update pacificAtlantic.py

--- pacificAtlantic.py
+++ pacificAtlantic.py
@@ -4,3 +4,5 @@ from typing import List
 
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        pac, atl = set(), set()
